8fc37c9f4fe3f7babcab9e5252e7c7764af03e6e
noppiii <mnoviantoanggoro@gmail.com>
2024-11-22 20:57:56 +0700

    init project

---
+++ src/main/java/com/wiyata/wiyata/backend/WiyataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wiyata.wiyata.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiyataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiyataBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiyata/wiyata/backend/WiyataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiyata.wiyata.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiyataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
